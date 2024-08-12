5a1647664ba128037bed349006c28bab1d1d37e2
Battsengel <batcengelbatzargal64@gmail.com>
2024-08-12 17:40:23 +0800

    atlas deer db tei bolov

--- index.js
+++ index.js
@@ -3,43 +3,38 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
-const {notFound,errorHandler}= require('./middleware/errorHandler');
+const { notFound, errorHandler } = require('./middleware/errorHandler');
+
 app.use(cors({
   origin: "*", 
-  methods: ["GET", "POST","PUT","DELETE"], 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
   credentials: true 
 }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// const Status = require("./routes/survey_status");
-// app.use("/api",Status);
-const Survey = require("./routes/survey")
-app.use("/api",Survey);
+const Survey = require("./routes/survey");
+app.use("/api", Survey);
 const Type = require("./routes/question_type");
-app.use("/api",Type);
+app.use("/api", Type);
 const Question = require("./routes/question");
-app.use("/api",Question);
-// const Qoption = require("./routes/question_options");
-// app.use("/api",Qoption);
+app.use("/api", Question);
 const User = require("./routes/users");
-app.use("/api",User);
+app.use("/api", User);
 const Response = require("./routes/response");
-app.use("/api",Response);
-// const Answer = require("./routes/answer");
-// app.use("/api",Answer);
+app.use("/api", Response);
 const Aop = require("./routes/answer_options");
-app.use("/api",Aop);
+app.use("/api", Aop);
 const Asuult = require("./routes/asuultavah");
-app.use("/api",Asuult);
+app.use("/api", Asuult);
 const Squestion = require("./routes/survey_questions");
-app.use("/api",Squestion);
-
+app.use("/api", Squestion);
 
 const connectToDB = async () => {
+
   try {
-    await mongoose.connect('mongodb://localhost:27017/SurveyDB', {});
+    await mongoose.connect('mongodb+srv://a:a@survey.pqwcm.mongodb.net/?retryWrites=true&w=majority&appName=survey');
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
@@ -47,10 +42,12 @@ const connectToDB = async () => {
   }
 };  
 connectToDB();
+
 const port = 3106;
 app.use(notFound);
-app.use(errorHandler)
-app.get("/" ,(req,res)=>{
+app.use(errorHandler);
+
+app.get("/", (req, res) => {
   res.status(200).send("Hello from the server!");
 });
 
