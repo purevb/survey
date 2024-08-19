19c70d7d3cccaeb95501a967f4a2b0a0fbd5a878
Battsengel <batcengelbatzargal64@gmail.com>
2024-08-19 15:13:06 +0800

    sda

--- controller/response.js
+++ controller/response.js
@@ -1,58 +1,57 @@
 const Res = require("../models/response");
 
-const postResponse= async(req,res)=>{
-    try{
-        const newRes = new Res(req.body);
-        await newRes.save().then((response)=>{
-            console.log(response);
-            res.status(200).json({msg:"Amjilttai",response:newRes});
-        })
-    }catch(error){
-        console.log(error);
-        res.status(500).json({msg:"aldaa garlaa"})
-    }
-}
-const getResponse = async(req,res)=>{
-    try{
-        await Res.find().then((response)=>{
-            console.log(response);
-            res.status(200).json({response:response})
-        })
+const postResponse = async (req, res) => {
+  try {
+    const newRes = new Res(req.body);
+    await newRes.save().then((response) => {
+      console.log(response);
+      res.status(200).json({ msg: "Amjilttai", response: newRes });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "aldaa garlaa" });
+  }
+};
+const getResponse = async (req, res) => {
+  try {
+    await Res.find().then((response) => {
+      console.log(response);
+      res.status(200).json({ response: response });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server Error", error: error.message });
+  }
+};
 
-    }catch(error){
-        console.log(error);
-        res.status(500).json({msg:"Aldaa"})
-    }
-}
-const search = async(req,res)=>{
-    try{
-        const id = req.params.id;
-        await Res.findById(id).then((response)=>{
-            console.log(response);
-            res.status(200).json({response:response});
-        })
-
-    }catch(error){
-        console.log(error);
-        res.status(500).json({msg:"Aldaa"})
-    }
-}
-const deleteResponse= async(req,res)=>{
-    try{
-        const id = req.params.id;
-        await Res.findByIdAndDelete(id).then((response)=>{
-            console.log(response);
-            res.status(500).json({msg:"amjilttai ustla",response:response});
-        })
-    }catch(error){
-        console.log(error);
-        res.status(500).json({msg:"Aldaa"})
-    }
-}
-module.exports={
-postResponse,
-getResponse,
-search,
-deleteResponse
-
-}
+const search = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Res.findById(id).then((response) => {
+      console.log(response);
+      res.status(200).json({ response: response });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Aldaa" });
+  }
+};
+const deleteResponse = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Res.findByIdAndDelete(id).then((response) => {
+      console.log(response);
+      res.status(500).json({ msg: "amjilttai ustla", response: response });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Aldaa" });
+  }
+};
+module.exports = {
+  postResponse,
+  update,
+  getResponse,
+  search,
+  deleteResponse,
+};
--- routes/response.js
+++ routes/response.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {getResponse,postResponse,search,deleteResponse} =require("../controller/response");
+const {
+  getResponse,
+  update,
+  postResponse,
+  search,
+  deleteResponse,
+} = require("../controller/response");
 
+router.post("/response", postResponse);
+router.put("/updateResponse/:id", update);
+router.get("/response", getResponse);
+router.get("/response/:id", search);
+router.delete("/response/:id", deleteResponse);
 
-router.post("/response",postResponse);
-router.get("/response",getResponse);
-router.get("/response/:id",search);
-router.delete("/response/:id",deleteResponse);
-
-module.exports=router;
+module.exports = router;
