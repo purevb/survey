5f05afb44124bddd07e92bb6eb61ba5f6314d9a8
Battsengel <batcengelbatzargal64@gmail.com>
2024-08-28 12:35:14 +0800

    hariult nemev ustgav

--- controller/question.js
+++ controller/question.js
@@ -2,18 +2,20 @@ const Ques = require("../models/question");
 
 const getQuestion = async (req, res) => {
   try {
-    await Ques.find().populate("questions_type_id").then((question) => {
-      //
-      console.log(question);
-      res.status(200).json({ question: question });
-    });
+    await Ques.find()
+      .populate("questions_type_id")
+      .then((question) => {
+        //
+        console.log(question);
+        res.status(200).json({ question: question });
+      });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "aldaatai bn" });
   }
 };
 
-const get= async (req, res) => {
+const get = async (req, res) => {
   try {
     await Ques.find().then((question) => {
       //
@@ -38,19 +40,16 @@ const postQuestion = async (req, res) => {
   }
 };
 
-
-
 const postQuestions = async (req, res) => {
   try {
-    const questioArray=req.body.questions
-    const response=await Ques.insertMany(questioArray);
+    const questioArray = req.body.questions;
+    const response = await Ques.insertMany(questioArray);
     res.status(200).json({ question: response });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "aldatai bnddaa" });
   }
 };
-
 
 const search = async (req, res) => {
   try {
@@ -78,34 +77,11 @@ const updateQuestion = async (req, res) => {
       console.log(updatedQuestionResult);
       res.status(200).json({ question: updatedQuestionResult });
     }
-  } catch (error) { 
+  } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "aldaaaaaa" });
   }
 };
-const updateAnswer = async (req, res) => {
-  try {
-    const questionId = req.params.questionId;
-    const answerId = req.params.answerId;
-    const updatedAnswerText = req.body.answer_text;
-    const question = await Ques.findById(questionId);
-
-    if (!question) {
-      return res.status(404).json({ msg: 'Question not found' });
-    }
-    const answer = question.answers.id(answerId);
-    if (!answer) {
-      return res.status(404).json({ msg: 'Answer not found' });
-    }
-    answer.answer_text = updatedAnswerText;
-    await question.save();
-    res.status(200).json({ msg: 'Answer updated successfully', question });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ msg: 'An error occurred' });
-  }
-};
-
 const deleteQuestion = async (req, res) => {
   try {
     const id = req.params.id;
@@ -119,6 +95,65 @@ const deleteQuestion = async (req, res) => {
   }
 };
 
+const addAnswerToQuestion = async (req, res) => {
+  try {
+    const questionId = req.params.questionId;
+    const newAnswer = req.body;
+    const question = await Ques.findById(questionId);
+    if (!question) {
+      return res.status(404).json({ msg: "Question not found" });
+    }
+    question.answers.push(newAnswer);
+    await question.save();
+    res.status(200).json({ msg: "Answer added successfully", question });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "An error occurred" });
+  }
+};
+const updateAnswer = async (req, res) => {
+  try {
+    const questionId = req.params.questionId;
+    const answerId = req.params.answerId;
+    const updatedAnswerText = req.body.answer_text;
+    const question = await Ques.findById(questionId);
+
+    if (!question) {
+      return res.status(404).json({ msg: "Question not found" });
+    }
+    const answer = question.answers.id(answerId);
+    if (!answer) {
+      return res.status(404).json({ msg: "Answer not found" });
+    }
+    answer.answer_text = updatedAnswerText;
+    await question.save();
+    res.status(200).json({ msg: "Answer updated successfully", question });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "An error occurred" });
+  }
+};
+const removeAnswerToQuestion = async (req, res) => {
+  try {
+    const questionId = req.params.questionId;
+    const answerId = req.params.answerId;
+    const question = await Ques.findById(questionId);
+    if (!question) {
+      return res.status(404).json({ msg: "Question not found" });
+    }
+    const answer = question.answers.id(answerId);
+    if (!answer) {
+      return res.status(404).json({ msg: "Answer not found" });
+    }
+    question.answers.pull(answerId);
+    await question.save();
+
+    res.status(200).json({ msg: "Answer removed successfully", question });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ msg: "An error occurred" });
+  }
+};
 
 module.exports = {
   updateAnswer,
@@ -128,5 +163,7 @@ module.exports = {
   search,
   updateQuestion,
   deleteQuestion,
-  get
+  get,
+  addAnswerToQuestion,
+  removeAnswerToQuestion,
 };
--- routes/question.js
+++ routes/question.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const router = express.Router();
+    const express = require("express");
+    const router = express.Router();
 
-const { get, updateAnswer, postQuestion, postQuestions, getQuestion, search, updateQuestion, deleteQuestion } = require("../controller/question.js");
+    const {removeAnswerToQuestion,addAnswerToQuestion, get, updateAnswer, postQuestion, postQuestions, getQuestion, search, updateQuestion, deleteQuestion } = require("../controller/question.js");
 
-router.post("/question", postQuestion);
-router.post("/questions", postQuestions);
-router.get("/question", getQuestion);
-router.get("/get", get);
-router.get("/question/:id", search);
-router.put("/question/:id", updateQuestion);
-router.put('/question/:questionId/answer/:answerId', updateAnswer);
-router.delete("/question/:id", deleteQuestion);
+    router.post("/question", postQuestion);
+    router.post("/questions", postQuestions);
+    router.post("/question/:questionId/answers", addAnswerToQuestion);
+    router.get("/question", getQuestion);
+    router.get("/get", get);
+    router.get("/question/:id", search);
+    router.put("/question/:id", updateQuestion);
+    router.put('/question/:questionId/answer/:answerId', updateAnswer);
+    router.delete("/question/:id", deleteQuestion);
+    router.delete("/question/:questionId/answers/:answerId", removeAnswerToQuestion);
 
-
-module.exports = router;
+    module.exports = router;
