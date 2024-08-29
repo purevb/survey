23c9471e58e653f6fa696e7d6bf6ea9be37ccc15
Battsengel <batcengelbatzargal64@gmail.com>
2024-08-29 19:04:38 +0800

    urrcllt ortson bn

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const UserService = require("../services/user_services");
 const router = express.Router();
 const {addSurveyId,loginUser,getUser,search,postUser}= require("../controller/users.js");
 router.get("/user",getUser);
